ab2ef59ba198c9b7933cbed289b0f77640a32600
jackAvilaP <arenca44@gmail.com>
2023-01-22 13:24:38 -0500

    change feed spinner

--- src/components/publication/Feed.jsx
+++ src/components/publication/Feed.jsx
@@ -3,19 +3,19 @@ import { useSelector } from "react-redux";
 import InfoHeader from "../private/InfoHeader";
 import SideBar from "../private/SideBar";
 import CardPublication from "./CardPublication";
-
+import SpinnerLoading from "./SpinnerLoading/SpinnerLoading";
 
 const Feed = () => {
   const loading = useSelector((state) => state.isloading);
-  const token = localStorage.getItem("token")
-  if(token){
-   console.log(true)
-  }
-  else{
-    console.log(false)
+  const token = localStorage.getItem("token");
+  if (token) {
+    console.log(true);
+  } else {
+    console.log(false);
   }
   return (
     <div>
+      {token &&<SpinnerLoading />}
       <InfoHeader />
       <section className="grid grid-cols-6 gap-3">
         <div className="col-start-1 col-end-5 p-2">
